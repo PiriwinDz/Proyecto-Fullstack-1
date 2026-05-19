6b43d046ffa8d43fc29daa029daca920b51493a0
José Contreras <joa.contrerass@duocuc.cl>
2026-05-19 12:25:17 -0400

    cambio LogroRepository.java

--- microservicio logros/powerapp-logros/src/main/java/cl/powerapp/logros/repository/LogroRepository.java
+++ microservicio logros/powerapp-logros/src/main/java/cl/powerapp/logros/repository/LogroRepository.java
@@ -1,8 +1,13 @@
 package cl.powerapp.logros.repository;
 
 import cl.powerapp.logros.model.Logro;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 public interface LogroRepository extends JpaRepository<Logro,Integer>{
+
+    List<Logro> findByUsuarioId(Long usuarioId);
 
  
 }
