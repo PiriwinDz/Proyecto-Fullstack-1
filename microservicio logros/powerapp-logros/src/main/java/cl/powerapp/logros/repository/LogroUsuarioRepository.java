0ca4a5da2a4581f428fb93855e693e3f7c74270c
José Contreras <joa.contrerass@duocuc.cl>
2026-05-19 12:23:49 -0400

    Update LogroUsuarioRepository.java

--- microservicio logros/powerapp-logros/src/main/java/cl/powerapp/logros/repository/LogroUsuarioRepository.java
+++ microservicio logros/powerapp-logros/src/main/java/cl/powerapp/logros/repository/LogroUsuarioRepository.java
@@ -1,10 +1,1 @@
-package cl.powerapp.logros.repository;
 
-import cl.powerapp.logros.model.LogroUsuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogroUsuarioRepository extends JpaRepository <LogroUsuario,Integer> {
-
-    
-    
-}
