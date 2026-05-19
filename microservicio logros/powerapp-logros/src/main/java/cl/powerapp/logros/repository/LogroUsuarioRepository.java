51c2bba31f407b76e9afc67fdec7836ad1791241
José Contreras <joa.contrerass@duocuc.cl>
2026-05-19 01:54:06 -0400

    agregando el microservicio de logros

---
+++ microservicio logros/powerapp-logros/src/main/java/cl/powerapp/logros/repository/LogroUsuarioRepository.java
@@ -0,0 +1,10 @@
+package cl.powerapp.logros.repository;
+
+import cl.powerapp.logros.model.LogroUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogroUsuarioRepository extends JpaRepository <LogroUsuario,Integer> {
+
+    
+    
+}
