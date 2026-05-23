84f5a3fcbd416577c409556a357c58523ed0848a
PiriwinDz <s.diaz4@duocuc.cl>
2026-05-23 17:13:03 -0400

    se agrego lo que faltaba

---
+++ microservicio-planes/Plan.java
@@ -0,0 +1,29 @@
+package com.example.microservicio.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "planes")
+public class Plan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private Double coste;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+}
