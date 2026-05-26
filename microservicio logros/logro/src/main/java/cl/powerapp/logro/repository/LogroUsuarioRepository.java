42f06e627b771d73b1de90e6b65e25fec9f5078f
José Contreras <joa.contrerass@duocuc.cl>
2026-05-26 02:05:29 -0400

    se edita LogroUsuarioRepository.java

--- microservicio logros/logro/src/main/java/cl/powerapp/logro/repository/LogroUsuarioRepository.java
+++ microservicio logros/logro/src/main/java/cl/powerapp/logro/repository/LogroUsuarioRepository.java
@@ -1,11 +1,17 @@
 package cl.powerapp.logro.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import cl.powerapp.logro.model.LogroUsuario;
 
-public interface LogroUsuarioRepository extends JpaRepository<LogroUsuario,Long>{
+public interface LogroUsuarioRepository
+        extends JpaRepository<LogroUsuario, Long>{
 
     List<LogroUsuario> findByUsuarioId(Long usuarioId);
 
+    boolean existsByUsuarioIdAndLogroId(
+            Long usuarioId,
+            Long logroId);
 }
