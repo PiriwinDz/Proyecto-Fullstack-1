bdec86da8778590decc4da35304e7cb5c180d546
José Contreras <joa.contrerass@duocuc.cl>
2026-05-26 13:13:18 -0400

    se eliminan comentarios EjercicioController.java

--- microservicio catalogo/src/main/java/com/example/catalogo/controller/EjercicioController.java
+++ microservicio catalogo/src/main/java/com/example/catalogo/controller/EjercicioController.java
@@ -22,7 +22,7 @@ public class EjercicioController {
 
     @PostMapping
     public ResponseEntity<Ejercicio> crear(@Valid @RequestBody EjercicioDTO dto) {
-        //  el código 201 CREATED
+       
         Ejercicio nuevoEjercicio = ejercicioService.guardarEjercicio(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoEjercicio);
     }
@@ -31,18 +31,18 @@ public class EjercicioController {
     public ResponseEntity<List<Ejercicio>> listarTodo() {
         List<Ejercicio> ejercicios = ejercicioService.listarTodo();
         
-        // 204 NO CONTENT si esta vacio
+        
         if (ejercicios.isEmpty()) {
             return ResponseEntity.noContent().build();
         }
         
-        // Código 200 OK  
+        
         return ResponseEntity.ok(ejercicios);
     }
     
     @GetMapping("/{id}")
     public ResponseEntity<Ejercicio> buscarPorId(@PathVariable Long id) {
-        // Manejo de 404 para recursos inexistentes
+        
         return ejercicioService.buscarPorId(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
