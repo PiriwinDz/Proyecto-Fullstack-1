0fdfdf29396060d305986ce47d0d7432115009cc
José Contreras <joa.contrerass@duocuc.cl>
2026-05-26 13:52:46 -0400

    se eliminan comentarios PlanController.java

--- microservicio-planes/src/main/java/com/example/microservicio/controller/PlanController.java
+++ microservicio-planes/src/main/java/com/example/microservicio/controller/PlanController.java
@@ -21,30 +21,30 @@ public class PlanController {
     @PostMapping
     public ResponseEntity<PlanResponseDTO> createPlan(@Valid @RequestBody PlanRequestDTO requestDTO) {
         PlanResponseDTO response = planService.createPlan(requestDTO);
-        return new ResponseEntity<>(response, HttpStatus.CREATED); // 201 CREATED
+        return new ResponseEntity<>(response, HttpStatus.CREATED); 
     }
 
     @GetMapping
     public ResponseEntity<List<PlanResponseDTO>> getAllPlanes() {
         List<PlanResponseDTO> responses = planService.getAllPlanes();
-        return ResponseEntity.ok(responses); // 200 OK
+        return ResponseEntity.ok(responses); 
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PlanResponseDTO> getPlanById(@PathVariable Long id) {
         PlanResponseDTO response = planService.getPlanById(id);
-        return ResponseEntity.ok(response); // 200 OK
+        return ResponseEntity.ok(response); 
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<PlanResponseDTO> updatePlan(@PathVariable Long id, @Valid @RequestBody PlanRequestDTO requestDTO) {
         PlanResponseDTO response = planService.updatePlan(id, requestDTO);
-        return ResponseEntity.ok(response); // 200 OK
+        return ResponseEntity.ok(response); 
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePlan(@PathVariable Long id) {
         planService.deletePlan(id);
-        return ResponseEntity.noContent().build(); // 204 NO CONTENT
+        return ResponseEntity.noContent().build(); 
     }
 }
