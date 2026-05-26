f0a79b7bde203ef3bcb7f9e7d82fdb363c7cd523
José Contreras <joa.contrerass@duocuc.cl>
2026-05-26 13:53:28 -0400

    se elmina comentario ErrorResponseDTO.java

--- microservicio-planes/src/main/java/com/example/microservicio/dto/ErrorResponseDTO.java
+++ microservicio-planes/src/main/java/com/example/microservicio/dto/ErrorResponseDTO.java
@@ -18,5 +18,5 @@ public class ErrorResponseDTO {
     private String error;
     private String message;
     private String path;
-    private List<String> details; // Lista para errores específicos, como los de validación
+    private List<String> details; 
 }
