edd79a94c8af0d76b1b2d388236a57d16d69816d
José Contreras <joa.contrerass@duocuc.cl>
2026-06-23 00:05:56 -0400

    cambio al test

--- microservicio pagoJC/pagos/pagos/src/test/java/com/powerApp/pagos/controller/PagoControllerTest.java
+++ microservicio pagoJC/pagos/pagos/src/test/java/com/powerApp/pagos/controller/PagoControllerTest.java
@@ -11,9 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,13 +24,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PagoController.class)
-@AutoConfigureMockMvc(addFilters = false)
 class PagoControllerTest {
 
         @Autowired
         private MockMvc mockMvc;
 
-        @MockBean
+        @MockitoBean
         private PagoService pagoService;
 
         @Autowired
