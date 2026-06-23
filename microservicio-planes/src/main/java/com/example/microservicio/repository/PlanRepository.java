c55bb49e6fdae2a118227b9eef055b76c6714256
José Contreras <joa.contrerass@duocuc.cl>
2026-06-22 21:46:15 -0400

    borrado todo porque agregué muchas cosas como para cambiar todo

--- microservicio-planes/src/main/java/com/example/microservicio/repository/PlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservicio.repository;
-
-import com.example.microservicio.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
